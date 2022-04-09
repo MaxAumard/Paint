7629846cf3bdbe740a5012d6316f296de426d004
jerem <jeremy-2000@hotmail.fr>
2022-04-09 16:43:12 +0200

    commit debut menu bar

--- graphics/menus/menuBar/MenuBar.java
+++ graphics/menus/menuBar/MenuBar.java
@@ -5,6 +5,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
@@ -28,16 +29,25 @@ public class MenuBar extends java.awt.MenuBar {
         menuBar.add(file);
 
 
-        JMenuItem newItem = new JMenuItem("New File");
-        newItem.setBackground(new Color(239, 239, 239));
-        newItem.setForeground(Color.black);
-        newItem.setMnemonic('N');
-        newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
+        JMenuItem newItem = new JMenuItem("New");
+        newItem.addActionListener(this::mnuNewListerner);
+        newItem.setBackground(new Color(239, 239, 239));//fond
+        newItem.setForeground(Color.black);//text
+        newItem.setMnemonic('N');//demande d'ouvrir le menu
+        newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));//pas besoin d'ouvrir le menu
         newItem.setIcon(setImageSize("icon/new.png"));
         newItem.setBorderPainted(false);
         file.add(newItem);
+        
+        JMenuItem openItem = new JMenuItem("Open File...");
+        openItem.setBackground(new Color(239, 239, 239));//fond
+        openItem.setForeground(Color.black);//text
+        openItem.setIcon(setImageSize("icon/open.png"));
+        openItem.setBorderPainted(false);
+        file.add(openItem);
 
-
+        file.addSeparator();
+        
         JMenuItem saveItem = new JMenuItem("Save");
         saveItem.setBackground(new Color(239, 239, 239));
         saveItem.setForeground(Color.black);
@@ -46,11 +56,22 @@ public class MenuBar extends java.awt.MenuBar {
         saveItem.setIcon(setImageSize("icon/save.png"));
         saveItem.setBorderPainted(false);
         file.add(saveItem); 
+        
+        JMenuItem saveasItem = new JMenuItem("Save as...");
+        saveasItem.setBackground(new Color(239, 239, 239));
+        saveasItem.setForeground(Color.black);
+        saveasItem.setIcon(setImageSize("icon/saveas.png"));
+        saveasItem.setBorderPainted(false);
+        file.add(saveasItem); 
     }
     public ImageIcon setImageSize(String path) throws IOException {
         return new ImageIcon(ImageIO.read(new File(path)).getScaledInstance(20,20,0));
     }
     public JMenuBar getMyJMenuBar(){
         return this.menuBar;
+    }
+    
+    private void mnuNewListerner(ActionEvent event) {
+    	System.out.println("Creation New file");
     }
 }
