106aad5ae453d1c71043c0277ce0e81f2b614cd3
Ag031 <rudeau.agathe@gmail.com>
2022-04-19 00:58:56 +0200

    sizeChooser debut

---
+++ graphics/menus/extensions/SizeChooser.java
@@ -0,0 +1,18 @@
+package graphics.menus.extensions;
+
+import java.awt.Color;
+
+public class SizeChooser {
+
+	int size;
+	
+	 public int getSizeChoosed() {
+	        return this.size;
+	    }
+
+	    public void setSizeChooseed(int size) {
+	        this.size = size;
+
+	    }
+	
+}
